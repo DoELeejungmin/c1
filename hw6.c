8101e7b3844bd1ba5762ad9587c0466eaf208698
DoELeejungmin <poi2478@naver.com>
2023-11-10 16:18:56 +0900

    add Text File [hw6.c]

---
+++ hw6.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() 
+{
+    int numbers[5];
+    printf("Please input five integers: ");
+
+    for (int i = 0; i < 5; i++) 
+    {
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("Odd numbers: ");
+    for (int i = 0; i < 5; i++) 
+    {
+        if (numbers[i] % 2 != 0) 
+        {
+            printf("%d ", numbers[i]);
+        }
+    }
+
+    printf("\nEven numbers: ");
+    for (int i = 0; i < 5; i++) 
+    {
+        if (numbers[i] % 2 == 0) 
+        {
+            printf("%d ", numbers[i]);
+        }
+    }
+
+    return 0;
+}
